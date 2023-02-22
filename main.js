f26d6c0daf486941b8cf74e0275db74e99a11962
catMeooww <110690475+catMeooww@users.noreply.github.com>
2023-02-22 17:48:15 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 var SpeechRecognition = window.webkitSpeechRecognition;
 var Recognition = new SpeechRecognition();
-var TextBox = document.getElementById("textbox");
 
 function start() {
     Recognition.start();
@@ -9,31 +8,34 @@ Recognition.onresult = function (event) {
     console.log(event);
 
     var content = event.results[0][0].transcript;
-    //TextBox.innerHTML = content;
+    document.getElementById("textbox").innerHTML = content;
     console.log(content);
     if (content == "tire minha selfie") {
         console.log("tirando selfie em 5s");
         speak();
     }
 }
+
 function speak() {
     var synth = window.speechSynthesis;
     speakData = "Tirando Minha Selfie em 5 segundos";
     utterThis = new SpeechSynthesisUtterance(speakData);
     synth.speak(utterThis);
-    Webcam.attach(camera);
+    Webcam.attach(document.getElementById("camera"));
     setTimeout(function () {
      takeSelfie();
+     console.log("selfie1")
      setTimeout(function () {
         takeSelfie2();
+        console.log("selfie2")
         setTimeout(function () {
             takeSelfie3();
+            console.log("selfie3")
            }, 1000);
        }, 1000);
     }, 5000);
 }
 
-camera = document.getElementById("camera");
 Webcam.set({
     width:360,
     height:250,
@@ -43,18 +45,18 @@ Webcam.set({
 function takeSelfie()
 {
     Webcam.snap(function(data_uri) {
-        document.getElementById("resultado1").innerHTML = '<img id="selfieImage"  style="box-shadow: 0 6px 8px 0 rgba(2, 2, 2, 0.2); src="'+data_uri+'"/>';
+        document.getElementById("resultado1").innerHTML = '<img id="selfieImage"  style="width:90%;height:90%;box-shadow: 0 6px 8px 0 rgba(2, 2, 2, 0.2); src="'+data_uri+'"/>';
     });
 }
 function takeSelfie2()
 {
     Webcam.snap(function(data_uri) {
-        document.getElementById("resultado2").innerHTML = '<img id="selfieImage"  style="box-shadow: 0 6px 8px 0 rgba(2, 2, 2, 0.2); src="'+data_uri+'"/>';
+        document.getElementById("resultado2").innerHTML = '<img id="selfieImage"  style="width:90%;height:90%;box-shadow: 0 6px 8px 0 rgba(2, 2, 2, 0.2); src="'+data_uri+'"/>';
     });
 }
 function takeSelfie3()
 {
     Webcam.snap(function(data_uri) {
-        document.getElementById("resultado3").innerHTML = '<img id="selfieImage"  style="box-shadow: 0 6px 8px 0 rgba(2, 2, 2, 0.2); src="'+data_uri+'"/>';
+        document.getElementById("resultado3").innerHTML = '<img id="selfieImage"  style="width:90%;height:90%;box-shadow: 0 6px 8px 0 rgba(2, 2, 2, 0.2); src="'+data_uri+'"/>';
     });
 }
